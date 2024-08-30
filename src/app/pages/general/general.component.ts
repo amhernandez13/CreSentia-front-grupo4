42866d329850cefb0320f6015f47f9c788ab0e3c
Nelson <lvnelson.via@gmail.com>
2024-08-30 11:37:11 -0500

    Rutas a terapias adulto

--- src/app/pages/general/general.component.ts
+++ src/app/pages/general/general.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-general',
   standalone: true,
-  imports: [],
+  imports: [ RouterLink],
   templateUrl: './general.component.html',
-  styleUrl: './general.component.css'
+  styleUrls: ['./general.component.css']
 })
 export class GeneralComponent {
-
+  
 }
